28d97c3b4e0a029cf409cfaba8a9428fdb89990c
orliac <etienne.orliac@unil.ch>
2021-08-24 15:13:02 +0200

    Aligning requirements with pycsou

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -5,8 +5,8 @@ module load gcc/8.4.0-cuda
 module load cuda/10.2.89
 module list
 
-# Asssumes miniconda is installed and available in $PATH
-# (~/.bashrc modified by installer)
+# Asssumes conda is installed and available in $PATH
+export PATH=$HOME/miniconda3/bin/:$PATH
 conda update -n base -c defaults conda --yes
 
 # Delete and recreate conda environment;
@@ -22,28 +22,68 @@ fi
 
 pwd
 conda env list
-
 source ./pypeline.sh --no_shell
 conda env list
+conda list
 
 python -V
-pip install --upgrade setuptools
-pip install cupy-cuda102
 
-git clone https://github.com/imagingofthings/pyFFS.git
-cd pyFFS/
-git checkout v1.0
-python3 setup.py develop
-cd ..
+#pip install -e . #EO: dead slow
+#pip uninstall .
 
-# Check out 'dev' branch, otherwise no fits.py under io/
-git clone https://github.com/imagingofthings/ImoT_tools.git
-cd ImoT_tools/
-#git checkout v1.0
-git checkout dev
-python3 setup.py develop
-cd ..
+#exit
+#pip install --upgrade setuptools
 
-git clone https://github.com/matthieumeo/pycsou.git
-pip install -e pycsou/
+# to wipe up 
+#pip freeze > req_delete_all.txt
+#pip uninstall -r req_delete_all.txt -y
 
+#pip install cupy-cuda102
+
+if [ 1 -eq 1 ]; then
+
+    if [ -d pyFFS ]; then
+        cd pyFFS
+        python3 setup.py develop --user --uninstall
+        cd ..
+        rm -rf pyFFS/
+    fi
+    git clone https://github.com/imagingofthings/pyFFS.git
+    cd pyFFS/
+    git checkout v1.0
+    python3 setup.py develop --user
+    cd ..
+    
+    # ImoT_tools: check out 'dev' branch, otherwise no fits.py under io/
+    if [ -d ImoT_tools ]; then
+        cd ImoT_tools
+        python3 setup.py develop --user --uninstall
+        cd ..
+        rm -rf ImoT_tools
+    fi
+    git clone https://github.com/imagingofthings/ImoT_tools.git
+    cd ImoT_tools/
+    #git checkout v1.0
+    git checkout dev
+    python3 setup.py develop --user
+    cd ..
+    
+    if [ -d pycsou ]; then
+        pip uninstall pycsou --yes
+        rm -rf pycsou
+    fi
+    git clone https://github.com/matthieumeo/pycsou.git
+    cd pycsou
+    git checkout tags/v1.0.5
+    cat requirements.txt
+    pip install -e .
+    cd ..
+fi
+
+echo;
+pwd
+ls -l
+echo
+
+python3 setup.py develop --user --uninstall
+python3 setup.py develop --user
