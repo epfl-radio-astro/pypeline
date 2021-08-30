22684bf8ae78062ce4091267ce908964d5d1618b
orliac <etienne.orliac@unil.ch>
2021-08-30 22:59:27 +0200

    Test access env

--- Jenkinsfile
+++ Jenkinsfile
@@ -23,17 +23,9 @@ pipeline {
                 echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
                 sh "echo Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
                 sh "echo UTC_TAG = ${env.UTC_TAG}"
-                script {
-                    def now = new Date()
-                    println now.format("yyyy-MM-dd'T'HH-mm-ss'Z'", TimeZone.getTimeZone('UTC'))
-                }
-
-                
-
-                //fails sh 'echo TAG_UNIXTIME = ${env.TAG_UNIXTIME}'
                 //sh 'srun --partition build --time 00-00:15:00 --qos gpu --gres gpu:1 --mem 40G ./jenkins/slurm_lofar_toothbrush_ps.sh'
                 //sh 'srun --partition build --time 00-00:15:00 --qos gpu --gres gpu:1 --mem 40G ./jenkins/slurm_test_fastsynthesizer.sh'
-                //sh 'srun --partition build --time 00-00:15:00 --qos gpu --gres gpu:1 --mem 40G ./jenkins/slurm_test_synthesizer.sh'
+                sh 'srun --partition build --time 00-00:15:00 --qos gpu --gres gpu:1 --mem 40G ./jenkins/slurm_test_synthesizer.sh'
                 //sh 'ls -rtl'
             }
         }
--- jenkins/slurm_test_synthesizer.sh
+++ jenkins/slurm_test_synthesizer.sh
@@ -5,6 +5,7 @@
 #SBATCH --qos gpu
 #SBATCH --gres gpu:1
 #SBATCH --mem 40G
+#SBATCH --cpus-per-task 4
 
 set -e
 
@@ -16,11 +17,38 @@ module list
 source pypeline.sh --no_shell
 which python
 python -V
+pip show pypeline
 
 hostname
 
-# Benchmarking
-time python "./benchmarking/test_synthesizer.py"
+echo "UTC_TAG = $UTC_TAG"
+
+exit 0
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export VECLIB_MAXIMUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+
+echo 
+
+# Debug
+#time python "./benchmarking/debug.py"
+#amplxe-cl -collect hotspots -strategy ldconfig:notrace:notrace -- ~/miniconda3/envs/pypeline/bin/python ./benchmarking/debug.py
+
+# Timing
+echo;echo;echo
+#time python "./benchmarking/test_synthesizer.py"
+time python "./benchmarking/test_synthesizer_cpu.py"
+
+# cProfile
+time python -m cProfile -o cpu.pstats "./benchmarking/test_synthesizer_cpu.py"
+
+exit 0
 
 # Profiling
+echo;echo;echo
 amplxe-cl -collect hotspots -strategy ldconfig:notrace:notrace -- ~/miniconda3/envs/pypeline/bin/python benchmarking/test_synthesizer.py
+amplxe-cl -collect hotspots -strategy ldconfig:notrace:notrace -- ~/miniconda3/envs/pypeline/bin/python benchmarking/test_synthesizer_cpu.py
