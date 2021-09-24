2170b3da04748beb617dd1bd92bc5cf11af35229
orliac <etienne.orliac@unil.ch>
2021-09-24 12:29:01 +0200

    Temporarily reduce test

--- jenkins/slurm_generic_synthesizer.sh
+++ jenkins/slurm_generic_synthesizer.sh
@@ -49,12 +49,16 @@ echo "PY_SCRIPT = $PY_SCRIPT"
 # Note: --outdir is omitted, no output is written on disk
 
 # Timing
+echo Timing
 time python $PY_SCRIPT ${TEST_ARCH} ${TEST_ALGO} --outdir $OUTPUT_DIR
 echo; echo
 
 # cProfile
+echo cProfile
 time python -m cProfile -o $OUTPUT_DIR/cProfile.out $PY_SCRIPT ${TEST_ARCH} ${TEST_ALGO}
 echo; echo
+
+exit 0
 
 # Nvprof
 nvprof -o $OUTPUT_DIR/nvvp.out python $PY_SCRIPT ${TEST_ARCH} ${TEST_ALGO}
