6a2bbf13d9b2db0ebc750aa5891d06b6d1f5519c
orliac <etienne.orliac@unil.ch>
2021-11-18 10:55:53 +0100

    fixup: add pip install locally pypeline

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -6,11 +6,13 @@ pwd
 
 # Install Miniconda in batch mode the first time
 # (step only required once)
-#bash ./Miniconda3-latest-Linux-x86_64.sh -b
-#source ~/miniconda3/bin/activate
-#conda init
-#echo "export PATH=\"~/miniconda3/bin:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc
+if [ 0 == 1 ]; then
+    bash ./Miniconda3-latest-Linux-x86_64.sh -b
+    source ~/miniconda3/bin/activate
+    conda init
+    echo "export PATH=\"~/miniconda3/bin:\$PATH\"" >> ~/.bashrc
+    source ~/.bashrc
+fi
 
 which conda
 
@@ -31,52 +33,51 @@ pip --version
 which python
 python -V
 
-# Install non-conda packages
-pip install cupy-cuda111
-pip install pycsou --no-deps
-pip install pyFFS --no-deps
+if [ 1 == 0 ]; then
+    # Install non-conda packages
+    pip install cupy-cuda111
+    pip install pycsou --no-deps
+    pip install pyFFS --no-deps
 
-# Install dev branch of ImoT_tools
-IMOT_TOOLS=ImoT_tools
-if [ -d $IMOT_TOOLS ]; then
-    cd $IMOT_TOOLS
-    if [ `git symbolic-ref --short HEAD` != 'dev' ]; then
-        echo "Fatal: $IMOT_TOOLS already existing but not on dev branch. Exit."
-        exit 1
+    # Install dev branch of ImoT_tools
+    IMOT_TOOLS=ImoT_tools
+    if [ -d $IMOT_TOOLS ]; then
+        cd $IMOT_TOOLS
+        if [ `git symbolic-ref --short HEAD` != 'dev' ]; then
+            echo "Fatal: $IMOT_TOOLS already existing but not on dev branch. Exit."
+            exit 1
+        fi
+    else
+        git clone https://github.com/imagingofthings/ImoT_tools.git
+        cd $IMOT_TOOLS
+        git checkout dev
     fi
-else
-    git clone https://github.com/imagingofthings/ImoT_tools.git
-    cd $IMOT_TOOLS
-    git checkout dev
+    pip install --no-deps .
+    cd ..
+
+    # Install FINUFTT (CPU) from source
+    # !!! GCC 8 not recommended !!! but fftw not available for GCC 9...
+    module load gcc fftw
+    if [ -d finufft ]; then
+        echo "A finufft directory already exits. Will clean, pull, and recompile."
+        cd finufft
+        make clean
+        git pull
+    else
+        git clone https://github.com/flatironinstitute/finufft.git
+        cd finufft
+    fi
+    # Only if you want to have debug symbol/info included in bin
+    echo "CXXFLAGS += -g -DFFTW_PLAN_SAFE" > make.inc
+    make test -j
+    ###make perftest
+    make python
+    cd ..
 fi
-pip install --no-deps .
-cd ..
-
-
-# Install FINUFTT (CPU) from source
-# !!! GCC 8 not recommended !!! but fftw not available for GCC 9...
-module load gcc fftw
-if [ -d finufft ]; then
-    echo "A finufft directory already exits. Will clean, pull, and recompile."
-    cd finufft
-    make clean
-    git pull
-    #git pull + recomp?
-    #rm -rf?
-else
-    git clone https://github.com/flatironinstitute/finufft.git
-    cd finufft
-fi
-# Only if you want to have debug symbol/info included in bin
-echo "CXXFLAGS += -g -DFFTW_PLAN_SAFE" > make.inc
-make test -j
-###make perftest
-make python
-cd ..
-
 
 # Install pypeline locally in editable mode
-pip install --no-deps -e .
+#pip install --no-deps -e .
+pip install --no-deps .
 
 
 # Export newly created environment (commenting out imot-tools)
