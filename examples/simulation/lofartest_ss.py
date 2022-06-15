fixed calculation on uvw matrix

examples/simulation/lofartest_ss.py
@@ -35,8 +35,8 @@ start_time = time.process_time()
 cl_WCS = ifits.wcs("/home/etolley/rascil_ska_sim/results_lofar_time/imaging_dirty.fits")
 ms_file = "/home/etolley/rascil_ska_sim/results_lofar_time/lofar-pipeline_simulation.ms"
 
-cl_WCS = ifits.wcs("/work/ska/lofar30MHz_256/lofar30MHz256-image.fits")
-ms_file = "/work/ska/lofar30MHz_256/lofar30MHz_t201806301100_SBH256.MS"
+#cl_WCS = ifits.wcs("/work/ska/lofar30MHz_256/lofar30MHz256-image.fits")
+#ms_file = "/work/ska/lofar30MHz_256/lofar30MHz_t201806301100_SBH256.MS"
 ms = measurement_set.SKALowMeasurementSet(ms_file) # stations 1 - N_station 
 out_str = "skalow_small"
 
@@ -181,9 +181,6 @@ plt.savefig("lofartest_standard_new")
 f_interp = (I_lsq_eq.data  # We need to transpose axes due to the FORTRAN
             .reshape(N_level, N_cl_lon, N_cl_lat)  # indexing conventions of the FITS standard.
             .transpose(0, 2, 1))
-#f_interp = I_lsq_eq.data 
-#f_interp = np.rot90(f_interp, 2, axes=(1,2))
-#f_interp = np.flip(f_interp, axis=2)
 I_lsq_eq_interp = s2image.WCSImage(np.sum(f_interp,axis=0), cl_WCS)
 I_lsq_eq_interp.to_fits('bluebild_ss_lofartest_combined-test.fits')
 I_lsq_eq_interp = s2image.WCSImage(f_interp, cl_WCS)

examples/simulation/skalow_nufft_small_fov.py
@@ -168,7 +168,8 @@ for t, f, S, uvw in ProgressBar(
     #UVW_baselines_t = (UVW[:, None, :] - UVW[None, ...])
     #print('bb uvw baselines',UVW_baselines_t.shape)
     UVW_baselines_t = uvw
-    #print('xyz',XYZ.shape) 
+    print('UVW_baselines_t',UVW_baselines_t) 
+
 
     #UVW_baselines_t = np.zeros((15, 15, 3))
     #UVW_baselines_t[np.triu_indices(15, 0)] = uvw
@@ -180,19 +181,34 @@ for t, f, S, uvw in ProgressBar(
 
     print(S.shape, uvw.shape)
 
+    print('\n raw visibilities',S)
+
+    plt.imshow(np.absolute(S.data))
+    plt.savefig("skalow_nufft_new_visraw_mag")
+    plt.imshow(np.angle(S.data))
+    plt.savefig("skalow_nufft_new_visraw_phi")
+
     W = ms.beamformer(XYZ, wl)
     G = gram(XYZ, W, wl)
     S, W = measurement_set.filter_data(S, W)
     W = W.data
     D, V, _ = I_dp(S, G)
     S_corrected = (W @ ((V @ np.diag(D)) @ V.transpose().conj())) @ W.transpose().conj()
+    #S_corrected = S.data
     gram_corrected_visibilities.append(S_corrected)
+    print('\n final visibilities',S_corrected)
+
+    plt.imshow(np.absolute(S_corrected))
+    plt.savefig("skalow_nufft_new_visfinal_mag")
+    plt.imshow(np.angle(S_corrected))
+    plt.savefig("skalow_nufft_new_visfinal_phi")
 
 UVW_baselines = np.stack(UVW_baselines, axis=0)
 ICRS_baselines = np.stack(ICRS_baselines, axis=0).reshape(-1, 3)
 gram_corrected_visibilities = np.stack(gram_corrected_visibilities, axis=0).reshape(-1)
 
 print('baselines', UVW_baselines.shape, 'visibilities', gram_corrected_visibilities.shape)
+print(gram_corrected_visibilities)
 
 # UVW_baselines = UVW_baselines.reshape((UVW_baselines.shape[0], -1, 3))
 #
@@ -217,7 +233,7 @@ print('baselines', UVW_baselines.shape, 'visibilities', gram_corrected_visibilit
 
 UVW_baselines=UVW_baselines.reshape(-1,3)
 
-plt.scatter(UVW_baselines_t[:,:,0], UVW_baselines_t[:,:,1])
+plt.plot(UVW_baselines_t[:,:,0], UVW_baselines_t[:,:,1], ".", color = "b", markersize = 0.2)
 plt.savefig("skalow_nufft_new_baselinesUV")
 
 w_correction = np.exp(1j * UVW_baselines[:, -1])

examples/simulation/testlofar_nufft_small_fov.py
@@ -156,13 +156,13 @@ ICRS_baselines = []
 gram_corrected_visibilities = []
 baseline_rescaling = 2 * np.pi / wl
 
-for t, f, S, uvw in ProgressBar(
-        ms.visibilities(channel_id=[channel_id], time_id=slice(0, None, None), column="DATA", return_UVW=True)
+for t, f, S in ProgressBar(
+        ms.visibilities(channel_id=[channel_id], time_id=slice(0, None, None), column="DATA", return_UVW=False)
 ):
     wl = constants.speed_of_light / f.to_value(u.Hz)
     XYZ = ms.instrument(t)
     #uvw = ms.UVW([channel_id], t)
-    print(uvw.shape)
+
     UVW = (uvw_frame.transpose() @ XYZ.data.transpose()).transpose()
     UVW_baselines_t = (UVW[:, None, :] - UVW[None, ...])
 
@@ -170,7 +170,6 @@ for t, f, S, uvw in ProgressBar(
     UVW_baselines.append(baseline_rescaling * UVW_baselines_t)
     ICRS_baselines.append(baseline_rescaling * ICRS_baselines_t)
 
-    print(uvw.shape)
     print(UVW_baselines[-1].shape)
     W = ms.beamformer(XYZ, wl)
     G = gram(XYZ, W, wl)

pypeline/phased_array/measurement_set.py
@@ -297,11 +297,14 @@ class MeasurementSet:
             beam_id_1 = sub_table.getcol("ANTENNA2")  # (N_entry,)
             data_flag = sub_table.getcol("FLAG")  # (N_entry, N_channel, 4)
             data = sub_table.getcol(column)  # (N_entry, N_channel, 4)
-            uvw = sub_table.getcol("UVW")
+            #print('\nraw data',data.shape, data[:,:,-1])
+            uvw = -1*sub_table.getcol("UVW")
 
             # We only want XX and YY correlations
             data = np.average(data[:, :, [0, 3]], axis=2)[:, channel_id]
             data_flag = np.any(data_flag[:, :, [0, 3]], axis=2)[:, channel_id]
+
+            #print('data after averaging',data)
 
             # Set broken visibilities to 0
             data[data_flag] = 0
@@ -341,14 +344,16 @@ class MeasurementSet:
             t = time.Time(sub_table.calc("MJD(TIME)")[0], format="mjd", scale="utc")
             f = self.channels["FREQUENCY"]
             beam_idx = pd.Index(beam_id, name="BEAM_ID")
-            print(beam_id, beam_idx)
+            #print(beam_id, beam_idx)
             for ch_id in channel_id:
+                #print("vis", S[ch_id])
                 v = _series2array(S[ch_id].rename("S", inplace=True))
                 visibility = vis.VisibilityMatrix(v, beam_idx)
+                #print("visibility", visibility)
                 if return_UVW:
                     UVW_baselines = np.zeros((15, 15, 3))
                     UVW_baselines[np.triu_indices(15, 0)] = uvw
-                    UVW_baselines[np.tril_indices(15, -1)] = np.transpose(UVW_baselines,(1,0,2))[np.tril_indices(15, -1)]
+                    UVW_baselines[np.tril_indices(15, -1)] = -1*np.transpose(UVW_baselines,(1,0,2))[np.tril_indices(15, -1)]
                     yield t, f[ch_id], visibility, UVW_baselines
                 else:  
                     yield t, f[ch_id], visibility