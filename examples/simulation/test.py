b4284a102d808163cc1351932986fa02b17f655d
dasarpan007 <arpan.das@epfl.ch>
2022-11-30 15:44:17 +0100

    test file added for profiling

--- examples/simulation/test.py
+++ examples/simulation/test.py
@@ -15,16 +15,18 @@ import numpy as np
 from time import perf_counter
 import scipy.constants as constants
 import pypeline.phased_array.bluebild.parameter_estimator as bb_pe
+import sys
+import nvtx
 
-# import rmm
-# pool = rmm.mr.PoolMemoryResource(
-#     rmm.mr.CudaMemoryResource(),
-#     # initial_pool_size=2**34,
-#     # maximum_pool_size=2**35
-# )
-# rmm.mr.set_current_device_resource(pool)
-# import rmm
-# cp.cuda.set_allocator(rmm.rmm_cupy_allocator)
+import rmm
+pool = rmm.mr.PoolMemoryResource(
+    rmm.mr.CudaMemoryResource(),
+    # initial_pool_size=2**34,
+    # maximum_pool_size=2**35
+)
+rmm.mr.set_current_device_resource(pool)
+import rmm
+cp.cuda.set_allocator(rmm.rmm_cupy_allocator)
 
 N_station = 37
 N_level = 4
@@ -42,30 +44,102 @@ data_col = 'DATA'
 
 casatime = []
 cudftime = []
-
-# for endtime in [1,10,100,1000]:
-#     print(endtime)
-#     time_start = perf_counter()
-#     # for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
-#     #     vis = cp.array(S.data)
-#     ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
-#     time_end = perf_counter()
-
-#     casatime.append(time_end-time_start)
-
-#     time_start = perf_counter()
-#     # for t, f, S in cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
-#     #     vis = S
-#     cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
-#     time_end = perf_counter()
-
-#     cudftime.append(time_end-time_start)
+N=1
+endtime = 5
+for i in range(N):
+    tic = perf_counter()
+    with nvtx.annotate("msviscpu", color="green"):
+        for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
+            vis = cp.asarray(S.data)
+            print(vis)
+    toc = perf_counter()
+    print(i, toc-tic)
     
-# print(casatime)
-# print(cudftime)
+print('--------------')
 
-endtime = [1,10,100,1000]
+for i in range(N):
+    tic = perf_counter()
+    with nvtx.annotate("cudfgpu", color="blue"):
+        for t,f,S in cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
+            vis = S.data
+    toc = perf_counter()
+    print(i, toc-tic)
+    
+sys.exit(0)
+    
+        
+        
+# for endtime in [100]:
+#     print(endtime)
+#     tic = perf_counter()
+#     obj = ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
+#     for t,f,S in obj:
+#         continue
+#     toc = perf_counter()
+#     print(toc-tic)
+    
+#     time_start = perf_counter()
+#     for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
+#         #vis = cp.array(S.data)
+#         continue
+#     time_end = perf_counter()
+#     print(time_end - time_start)
+    
+#     time_start = perf_counter()
+#     for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
+#         #vis = cp.array(S.data)
+#         continue
+#     time_end = perf_counter()
+#     print(time_end - time_start)
+    
+#     tic = perf_counter()
+#     obj = ms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
+#     print(obj)
+#     for t,f,S in obj:
+#         continue
+#     toc = perf_counter()
+#     print(toc-tic)
+    
+#     #time_end = perf_counter()
 
-for item in endtime:
-    for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, item, 1), column="DATA"):
-        vis = cp.array(S.data)
+
+#     time_start = perf_counter()
+#     for t, f, S in cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA"):
+#         continue
+#     #cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
+#     time_end = perf_counter()
+
+#     print(time_end-time_start)
+    
+#     time_start = perf_counter()
+#     obj = cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
+#     print(obj)
+#     time_end = perf_counter()
+
+#     print(time_end-time_start)
+    
+#     time_start = perf_counter()
+#     obj = cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, endtime, 1), column="DATA")
+#     for t, f, S in obj:
+#         continue
+#     time_end = perf_counter()
+
+#     print(time_end-time_start)
+    
+    
+    
+#print(casatime)
+#print(cudftime)
+
+
+# ############ For profiling
+
+# endtime = [1000]
+
+# # for item in endtime:
+# #     for t,f,S in ms.visibilities(channel_id=[channel_id], time_id=slice(0, item, 1), column="DATA"):
+# #         vis = cp.array(S.data)
+
+# for item in endtime:
+#     for t, f, S in cudfms.visibilities(channel_id=[channel_id], time_id=slice(0, item, 1), column="DATA"):
+#         vis = S
