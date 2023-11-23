Bug fix: XYZ mean centering in double precision when using SS in SP

pypeline/phased_array/bluebild/imager/spatial_domain.py
@@ -211,6 +211,10 @@ class Spatial_IMFS_Block(bim.IntegratingMultiFieldSynthesizerBlock):
             (2, N_level, N_px) field statistics.
         """
 
+        # Reduce XYZ while in DP
+        assert(XYZ.dtype == np.float64)
+        XYZ = XYZ - np.mean(XYZ, axis=0)
+        
         XYZ = XYZ.astype(self._fp, copy=False)
         D   = D.astype(self._fp, copy=False)
         V   = V.astype(self._cp, copy=False)
@@ -230,7 +234,6 @@ class Spatial_IMFS_Block(bim.IntegratingMultiFieldSynthesizerBlock):
             tic = perf_counter()
             assert self._synthesizer._grid.dtype == self._fp, f'_grid {self._grid.dtype} not of expected type {self._fp}'
             stat_std = self._synthesizer(V, XYZ, W)
-                        
             assert stat_std.dtype == self._fp, f'stat_std {stat_std.dtype} not of expected type {self._fp}'
 
             # get result from GPU